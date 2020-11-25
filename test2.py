3ebc16bd54ccdf59bbe6ed15ee5d37aaa2bb449e
Javier <javier.barrera.152@my.csun.edu>
2020-11-24 20:51:38 -0800

    print to screen

--- test2.py
+++ test2.py
@@ -1,5 +1,7 @@
 import Adafruit_DHT
+import I2C_LCD_driver
 
+mylcd = I2C_LCD_driver.lcd()
 sensor=Adafruit_DHT.DHT22
 
 gpio = 17
@@ -8,5 +10,8 @@ humidity, tempterature = Adafruit_DHT.read_retry(sensor, gpio)
 
 if humidity is not None and tempterature is not None:
     print('Temp={0:0.1f}*C Humidity={1:0.1f}%'.format(tempterature, humidity))
+    mylcd.lcd_display_string(('Temp={0:0.1f}*C'.format(tempterature)),1)
+    mylcd.lcd_display_string(('Humidity={0:0.1f}%'.format(humidity)),2)
+
 else:
     print('Failed to get reading. Try again!')

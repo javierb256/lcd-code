bd0d1d5c393fd41274903dba4d60f202d0da9d4d
Javier <javier.barrera.152@my.csun.edu>
2020-11-24 22:49:22 -0800

    added intervals

--- reading_to_screen.py
+++ reading_to_screen.py
@@ -12,10 +12,12 @@ gpio = 17
 
 humidity, tempterature = Adafruit_DHT.read_retry(sensor, gpio)
 
-if humidity is not None and tempterature is not None:
-    print('Temp={0:0.1f}*C Humidity={1:0.1f}%'.format(tempterature, humidity))
-    mylcd.lcd_display_string(('Temp={0:0.1f}*C'.format(tempterature)),1)
-    mylcd.lcd_display_string(('Humidity={0:0.1f}%'.format(humidity)),2)
+while true:
+    if humidity is not None and tempterature is not None:
+        print('Temp={0:0.1f}*C Humidity={1:0.1f}%'.format(tempterature, humidity))
+        mylcd.lcd_display_string(('Temp={0:0.1f}*C'.format(tempterature)),1)
+        mylcd.lcd_display_string(('Humidity={0:0.1f}%'.format(humidity)),2)
 
-else:
-    print('Failed to get reading. Try again!')
+    else:
+        print('Failed to get reading. Try again!')
+    sleep(5)
